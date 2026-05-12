a4587863b2fb55b4c25709ad194426bcfa85eeff
amaya6498551-art <amaya6498551@gmail.com>
2026-05-12 14:33:41 -0600

    Actualización del segundo parcial

---
+++ script.js
@@ -0,0 +1,30 @@
+function Mostrar_Mensaje() {
+    let mensaje = document.getElementById("mensaje");
+
+    mensaje.innerHTML= "Cuida y protege a los animales";
+    mensaje.style.display = "block";
+
+    setTimeout(() => {
+        mensaje.style.opacity ="1";
+        mensaje.style.transform ="translateY(0)";
+    }, 50);
+
+
+}
+function Mostrar_Mensaje2() {
+    let mensaje = document.getElementById("mensaje2");
+
+    mensaje.innerHTML= "Hola bienvenid@ a mi pagina";
+    mensaje.style.display = "block";
+
+    setTimeout(() => {
+        mensaje.style.opacity ="1";
+        mensaje.style.transform ="translateY(0)";
+    }, 50);
+
+}
+
+
+function cambiarModo(){
+    document.body.classList.toggle("oscuro");
+}
